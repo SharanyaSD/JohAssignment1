9789d1c4ddad8d11c0416a849aa7c312dc16abd6
Sharanya <datrangesharanya@gmail.com>
2024-01-24 14:56:01 +0530

    Modified A6-question1.go

--- A6question1.go
+++ A6question1.go
@@ -1,5 +1,8 @@
+
 //Sharanya Datrange
 package main
+
+
 import (
 	"fmt"
 )
@@ -24,27 +27,15 @@ func SliceInput() []int {
 }
 
 func accessSlice(MySlice []int) {
-	//MySlice := [] int {2,4,6,8,10,12,14,16,18,20}
 	var index int
 	fmt.Println("Enter Input: ")
 	fmt.Scanln(&index)
-	found:=false
-	for i:=0;i<len(MySlice);i++ {
-		if index == i {
-			fmt.Printf("item %v, value %v ",i,MySlice[i])
-			found =true
-			break
+	defer func(){
+		if r:=recover(); r!=nil {
+			fmt.Printf("internal error: %v\n",r)
 		}
-	}
-	if !found {
-		defer func(){
-			if r:=recover(); r!=nil {
-				fmt.Printf("internal error: %v\n",r)
-			}
-		} ()
-		panic("Index out of bound")
-		
-	}
+	} ()
+	fmt.Printf("item %v, value %v ",index,MySlice[index])
 	
 }
 
@@ -52,20 +43,20 @@ func accessSlice(MySlice []int) {
 
 sharanya@sharanya-ASUS-TUF-Gaming-A15-FA506IHRB-FA506IHRZ:~/Desktop/MyGoPracs$ go run A6question1.go
 Enter the size of the slice:
-5
-Enter 5 elements for the slice:
-Element 0: 10
-Element 1: 20
-Element 2: 30
-Element 3: 40
-Element 4: 50
+3
+Enter 3 elements for the slice:
+Element 0: 1
+Element 1: 2
+Element 2: 3
 Enter Input: 
 9
-internal error: Index out of bound
+internal error: runtime error: index out of range [9] with length 3
 Testing Panic and Recover
+sharanya@sharanya-ASUS-TUF-Gaming
 
 
 
 */
+
 
 
